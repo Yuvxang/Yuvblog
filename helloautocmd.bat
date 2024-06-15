9fdb9696bfafc15200a3b1750da47f4e03a147f8
Yuvxang <1692026099@qq.com>
2024-06-15 08:13:20 +0800

    Skip render some pages

--- helloautocmd.bat
+++ helloautocmd.bat
@@ -1,5 +1,5 @@
 @echo off
 start cmd /k "git add ."
-start cmd /k "git commit -m "try cmd automation""
+start cmd /k "git commit -m "Skip render some pages""
 start cmd /k "git push"
 echo push end!
