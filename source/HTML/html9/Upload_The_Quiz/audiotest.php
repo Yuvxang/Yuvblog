ec7595188c84a532c8c6d3daf9f4dc4bade1405d
Yuvxang <1692026099@qq.com>
2024-06-16 23:11:43 +0800

    Mamba out!

---
+++ source/HTML/html9/Upload_The_Quiz/audiotest.php
@@ -0,0 +1,25 @@
+<?php
+    echo "<img src=\"kobe.jpeg\" style=\"width:300px;\"><br/>";
+?>
+
+让牢大对你说<?php echo $_POST["text"]; ?>!
+<br/>
+
+<?php
+$words = $_POST["text"];
+if(strtolower($words) == "man") {
+    echo "<audio src=\"man.mp3\" id=\"dd\" autoplay=\"autoplay\" controls=\"controls\"></audio>";
+}
+else if(strtolower($words) == "what can i say") {
+    echo "<audio src=\"wcis.mp3\" id=\"dd\" autoplay=\"autoplay\" controls=\"controls\"></audio>";
+}
+else if(strtolower($words) == "mamba out") {
+    echo "<audio src=\"mambaout.mp3\" id=\"dd\" autoplay=\"autoplay\" controls=\"controls\"></audio>";
+}
+else if(strtolower($words) == "man! what can i say") {
+    echo "<audio src=\"mwcis.mp3\" id=\"dd\" autoplay=\"autoplay\" controls=\"controls\"></audio>";
+}
+else {
+    echo "<audio src=\"whole.mp3\" id=\"dd\" autoplay=\"autoplay\" controls=\"controls\"></audio>";
+}
+?>
